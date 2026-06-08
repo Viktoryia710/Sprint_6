871acee9e37bcc46f2cca8f32ecd8fe67d0527a3
Viktoryia Zhuk <viktoryia.zhuk@gmail.com>
2026-06-08 19:32:51 +0800

    Внесены исправления

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,11 @@
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
-from locators.locators import MainPageLocators
+from locators.locators import MainPageLocators  # Добавили импорт локаторов!
+from config import Config
 
-class MainPage(BasePage):
+class MainPage(BasePage):  # Объявляем класс
+
     def accept_cookies(self):
         try:
             cookie_btn = self.wait.until(EC.element_to_be_clickable(MainPageLocators.COOKIE_BUTTON))
@@ -17,22 +20,33 @@ class MainPage(BasePage):
         button = self.find_element(MainPageLocators.BOTTOM_ORDER_BUTTON)
         self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", button)
         self.driver.execute_script("arguments[0].click();", button)
-        
-    def get_faq_answer_text(self, index):
-        question_locator = MainPageLocators.faq_question(index)
-        question_element = self.wait.until(EC.presence_of_element_located(question_locator))
-        
-        self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", question_element)
-        
-        self.driver.execute_script("arguments[0].click();", question_element)
-        
-        answer_locator = MainPageLocators.faq_answer(index)
-        answer_element = self.wait.until(EC.visibility_of_element_located(answer_locator))
-        
-        return answer_element.text
 
     def click_scooter_logo(self):
         self.click_element(MainPageLocators.SCOOTER_LOGO)
 
     def click_yandex_logo(self):
         self.click_element(MainPageLocators.YANDEX_LOGO)
+
+    def get_current_url(self):
+        return self.driver.current_url
+
+    def switch_to_new_tab(self):
+        WebDriverWait(self.driver, 10).until(EC.number_of_windows_to_be(2))
+        new_window = self.driver.window_handles[1]
+        self.driver.switch_to.window(new_window)
+
+    def wait_for_redirect_from_base_url(self):
+        WebDriverWait(self.driver, 10).until(
+            lambda driver: Config.BASE_URL not in driver.current_url
+        )
+    
+    def get_faq_answer_text(self, index):
+        question_locator = MainPageLocators.faq_question(index)
+        question_element = self.wait.until(EC.element_to_be_clickable(question_locator))
+        
+        self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", question_element)
+        self.driver.execute_script("arguments[0].click();", question_element)
+        
+        answer_locator = MainPageLocators.faq_answer(index)
+        answer_element = self.wait.until(EC.visibility_of_element_located(answer_locator))
+        return answer_element.text
--- tests/test_logo.py
+++ tests/test_logo.py
@@ -1,6 +1,5 @@
 import pytest
 import allure
-from selenium.webdriver.support import expected_conditions as EC
 from pages.main_page import MainPage
 from config import Config
 
@@ -23,11 +22,11 @@ class TestLogos:
             main_page.click_scooter_logo()
             
         with allure.step("Проверяем, что текущий URL соответствует главной странице"):
-            assert driver.current_url == Config.BASE_URL
+            assert main_page.get_current_url() == Config.BASE_URL
 
     @allure.title("Клик на логотип 'Яндекс' перенаправляет на внешнюю платформу")
     @allure.description("Тест кликает по логотипу 'Яндекс', переключается на открывшуюся вкладку и проверяет редирект.")
-    def test_click_yandex_logo_redirects_to_dzen(self, driver, wait):
+    def test_click_yandex_logo_redirects_to_dzen(self, driver):
         main_page = MainPage(driver)
         
         with allure.step("Принимаем куки"):
@@ -36,16 +35,11 @@ class TestLogos:
         with allure.step("Кликаем по логотипу 'Яндекс'"):
             main_page.click_yandex_logo()
         
-        with allure.step("Ожидаем появление второй вкладки браузера"):
-            wait.until(EC.number_of_windows_to_be(2))
-        
         with allure.step("Переключаемся на новую вкладку"):
-            new_window = driver.window_handles[1]
-            driver.switch_to.window(new_window)
+            main_page.switch_to_new_tab()
         
-        with allure.step("Ожидаем старт редиректа"):
-            import time
-            time.sleep(3)
+        with allure.step("Ожидаем редирект (уход с базового домена)"):
+            main_page.wait_for_redirect_from_base_url()
         
         with allure.step("Проверяем, что мы покинули домен 'Самоката'"):
-            assert Config.BASE_URL not in driver.current_url
+            assert Config.BASE_URL not in main_page.get_current_url()
